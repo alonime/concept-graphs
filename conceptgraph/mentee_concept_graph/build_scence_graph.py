1a7dde55d26003c614d8db7daeaebcfc4d795f1b
Lior <aloni@menteebot.com>
2024-06-23 11:16:49 +0300

    wip

--- conceptgraph/mentee_concept_graph/build_scence_graph.py
+++ conceptgraph/mentee_concept_graph/build_scence_graph.py
@@ -124,6 +124,35 @@ class SceneGraphConfigs:
 
     save_detections = True
 
+    build_spetial_edges: bool = True
+    mask_area_threshold: float = 25   # mask with pixel area less than this will be skipped
+    mask_conf_threshold: float =0.2   # mask with lower confidence score will be skipped
+    max_bbox_area_ratio: float = 1.0  # boxes with larger areas than this will be skipped
+    min_points_threshold: int = 16    # projected and sampled pcd with less points will be skipped
+    spatial_sim_type: Literal["iou", "giou", "overlap"] = "iou" # "iou", "giou", "overlap"
+    obj_pcd_max_points: 5000 
+    """ Determines the maximum point count for object point clouds; exceeding this triggers downsampling to approx max points. Set to -1 to disable """
+
+    # point cloud processing
+    downsample_voxel_size: float = 0.025
+    dbscan_remove_noise: bool = True
+    dbscan_eps: float = 0.05
+    dbscan_min_points: float = 10
+
+    phys_bias: float = 0.0
+    match_method: Literal["sep_thresh", "sim_sum"] =  "sep_thresh"
+    # Only when match_method=="sep_thresh"
+    semantic_threshold: float = 0.5
+    physical_threshold: float = 0.5
+    # Only when match_method=="sim_sum"
+    sim_threshold: float = 0
+
+    denoise_interval: int = 5           # Run DBSCAN every k frame. This operation is heavy
+    filter_interval: int = 5            # Filter objects that have too few associations or are too small
+    merge_interval: int = 5             # Merge objects based on geometric and semantic similarity
+    run_denoise_final_frame: bool = True
+    run_filter_final_frame: bool = True
+    run_merge_final_frame: bool = True
 
 
 
@@ -265,15 +294,21 @@ class SceneGraph:
             
 
             # Make the edges
-            labels, edges, edge_image, captions = make_vlm_edges(image, curr_det, obj_classes, detection_class_labels, det_exp_vis_path, rgb_path, cfg.make_edges, openai_client)
-            # labels, edges, edge_image = make_vlm_edges(image, curr_det, obj_classes, detection_class_labels, det_exp_vis_path, rgb_path, False, None)
+            labels, edges, edge_image, captions = make_vlm_edges(image, 
+                                                                 curr_det, 
+                                                                 self.obj_classes, 
+                                                                 detection_class_labels, 
+                                                                 results_dir_vis, 
+                                                                 rgb_path, 
+                                                                 self.configs.build_spetial_edges, 
+                                                                 self.openai_client)
 
             
             image_crops, image_feats, text_feats = compute_clip_features_batched(
-                image_rgb, curr_det, clip_model, clip_preprocess, clip_tokenizer, obj_classes.get_classes_arr(), cfg.device)
+                image_rgb, curr_det, self.clip_model, self.clip_preprocess, self.clip_tokenizer, self.obj_classes.get_classes_arr(), self.configs.device)
 
             # increment total object detections
-            tracker.increment_total_detections(len(curr_det.xyxy))
+            self.tracker.increment_total_detections(len(curr_det.xyxy))
 
             # Save results
             # Convert the detections to a dict. The elements are in np.array
@@ -283,7 +318,7 @@ class SceneGraph:
                 "confidence": curr_det.confidence,
                 "class_id": curr_det.class_id,
                 "mask": curr_det.mask,
-                "classes": obj_classes.get_classes_arr(),
+                "classes": self.obj_classes.get_classes_arr(),
                 "image_crops": image_crops,
                 "image_feats": image_feats,
                 "text_feats": text_feats,
@@ -295,45 +330,51 @@ class SceneGraph:
             raw_grounded_obs = results
 
             # save the detections if needed
-            if cfg.save_detections:
+            if self.configs.save_detections:
 
-                vis_save_path = (det_exp_vis_path / rgb_path.name).with_suffix(".jpg")
+                vis_save_path = (results_dir_vis / rgb_path.name).with_suffix(".jpg")
                 # Visualize and save the annotated image
-                annotated_image, labels = vis_result_fast(image, curr_det, obj_classes.get_classes_arr())
+                annotated_image, labels = vis_result_fast(image, curr_det, self.obj_classes.get_classes_arr())
                 cv2.imwrite(str(vis_save_path), annotated_image)
 
                 depth_image_rgb = cv2.normalize(depth_array, None, 0, 255, cv2.NORM_MINMAX)
                 depth_image_rgb = depth_image_rgb.astype(np.uint8)
                 depth_image_rgb = cv2.cvtColor(depth_image_rgb, cv2.COLOR_GRAY2BGR)
-                annotated_depth_image, labels = vis_result_fast_on_depth(depth_image_rgb, curr_det, obj_classes.get_classes_arr())
+                annotated_depth_image, labels = vis_result_fast_on_depth(depth_image_rgb, curr_det, self.obj_classes.get_classes_arr())
                 cv2.imwrite(str(vis_save_path).replace(".jpg", "_depth.jpg"), annotated_depth_image)
                 cv2.imwrite(str(vis_save_path).replace(".jpg", "_depth_only.jpg"), depth_image_rgb)
-                save_detection_results(det_exp_pkl_path / vis_save_path.stem, results)
+                save_detection_results(results_dir_detections / vis_save_path.stem, results)
         
             # get pose, this is the untrasformed pose.
-            unt_pose = dataset.poses[frame_idx]
+            unt_pose = self.dataset.poses[frame_idx]
             unt_pose = unt_pose.cpu().numpy()
 
             # Don't apply any transformation otherwise
             adjusted_pose = unt_pose
             
-            prev_adjusted_pose = orr_log_camera(intrinsics, adjusted_pose, prev_adjusted_pose, cfg.image_width, cfg.image_height, frame_idx)
-            
-            orr_log_rgb_image(rgb_path)
-            orr_log_annotated_image(rgb_path, det_exp_vis_path)
-            orr_log_depth_image(depth_tensor)
-            orr_log_vlm_image((results_dir_vis / rgb_path.name).with_suffix(".jpg"))
-            orr_log_vlm_image((results_dir_vis / rgb_path.name).with_suffix("w_edges.jpg"), label="w_edges")
+            if self.viz is not None:
+                prev_adjusted_pose = orr_log_camera(intrinsics,
+                                                    adjusted_pose,
+                                                    prev_adjusted_pose,
+                                                    self.dataset_config.camera_params.image_width,
+                                                    self.dataset_config.camera_params.image_height,
+                                                    frame_idx)
+                orr_log_rgb_image(rgb_path)
+                orr_log_annotated_image(rgb_path, vis_save_path)
+                orr_log_depth_image(depth_tensor)
+                orr_log_vlm_image((results_dir_vis / rgb_path.name).with_suffix(".jpg"))
+                orr_log_vlm_image((results_dir_vis / rgb_path.name).with_suffix("w_edges.jpg"), label="w_edges")
+
 
             # resize the observation if needed
             resized_grounded_obs = resize_gobs(raw_grounded_obs, image_rgb)
             # filter the observations
             filtered_grounded_obs = filter_gobs(resized_grounded_obs, image_rgb, 
-                skip_bg=cfg.skip_bg,
-                BG_CLASSES=obj_classes.get_bg_classes_arr(),
-                mask_area_threshold=cfg.mask_area_threshold,
-                max_bbox_area_ratio=cfg.max_bbox_area_ratio,
-                mask_conf_threshold=cfg.mask_conf_threshold,
+                skip_bg=self.configs.skip_bg,
+                BG_CLASSES=self.obj_classes.get_bg_classes_arr(),
+                mask_area_threshold=self.configs.mask_area_threshold,
+                max_bbox_area_ratio=self.configs.max_bbox_area_ratio,
+                mask_conf_threshold=self.configs.mask_conf_threshold,
             )
 
             grounded_obs = filtered_grounded_obs
@@ -350,28 +391,28 @@ class SceneGraph:
                 cam_K=intrinsics.cpu().numpy()[:3, :3],  # Camera intrinsics
                 image_rgb=image_rgb,
                 trans_pose=adjusted_pose,
-                min_points_threshold=cfg.min_points_threshold,
-                spatial_sim_type=cfg.spatial_sim_type,
-                obj_pcd_max_points=cfg.obj_pcd_max_points,
-                device=cfg.device,
+                min_points_threshold=self.configs.min_points_threshold,
+                spatial_sim_type=self.configs.spatial_sim_type,
+                obj_pcd_max_points=self.configs.obj_pcd_max_points,
+                device=self.configs.device,
             )
 
             for obj in obj_pcds_and_bboxes:
                 if obj:
                     obj["pcd"] = init_process_pcd(
                         pcd=obj["pcd"],
-                        downsample_voxel_size=cfg["downsample_voxel_size"],
-                        dbscan_remove_noise=cfg["dbscan_remove_noise"],
-                        dbscan_eps=cfg["dbscan_eps"],
-                        dbscan_min_points=cfg["dbscan_min_points"],
+                        downsample_voxel_size=self.configs.downsample_voxel_size,
+                        dbscan_remove_noise=self.configs.dbscan_remove_noise,
+                        dbscan_eps=self.configs.dbscan_eps,
+                        dbscan_min_points=self.configs.dbscan_min_points,
                     )
                     obj["bbox"] = get_bounding_box(
-                        spatial_sim_type=cfg['spatial_sim_type'], 
+                        spatial_sim_type=self.configs.spatial_sim_type, 
                         pcd=obj["pcd"],
                     )
 
             detection_list = make_detection_list_from_pcd_and_gobs(
-                obj_pcds_and_bboxes, grounded_obs, rgb_path, obj_classes, frame_idx
+                obj_pcds_and_bboxes, grounded_obs, rgb_path, self.obj_classes, frame_idx
             )
 
             if len(detection_list) == 0: # no detections, skip
@@ -382,26 +423,26 @@ class SceneGraph:
             # then continue, no need to match or merge
             if len(objects) == 0:
                 objects.extend(detection_list)
-                tracker.increment_total_objects(len(detection_list))
-                owandb.log({
-                        "total_objects_so_far": tracker.get_total_objects(),
+                self.tracker.increment_total_objects(len(detection_list))
+                self.owandb.log({
+                        "total_objects_so_far": self.tracker.get_total_objects(),
                         "objects_this_frame": len(detection_list),
                     })
                 continue 
 
             ### compute similarities and then merge
             spatial_sim = compute_spatial_similarities(
-                spatial_sim_type=cfg['spatial_sim_type'], 
+                spatial_sim_type=self.configs.spatial_sim_type, 
                 detection_list=detection_list, 
                 objects=objects,
-                downsample_voxel_size=cfg['downsample_voxel_size']
+                downsample_voxel_size=self.configs.downsample_voxel_size
             )
 
             visual_sim = compute_visual_similarities(detection_list, objects)
 
             agg_sim = aggregate_similarities(
-                match_method=cfg['match_method'], 
-                phys_bias=cfg['phys_bias'], 
+                match_method=self.configs.match_method, 
+                phys_bias=self.configs.phys_bias, 
                 spatial_sim=spatial_sim, 
                 visual_sim=visual_sim
             )
@@ -409,7 +450,7 @@ class SceneGraph:
             # Perform matching of detections to existing objects
             match_indices = match_detections_to_objects(
                 agg_sim=agg_sim, 
-                detection_threshold=cfg['sim_threshold']  # Use the sim_threshold from the configuration
+                detection_threshold=self.configs.sim_threshold  # Use the sim_threshold from the configuration
             )
 
             # Now merge the detected objects into the existing objects based on the match indices
@@ -417,52 +458,46 @@ class SceneGraph:
                 detection_list=detection_list, 
                 objects=objects, 
                 match_indices=match_indices,
-                downsample_voxel_size=cfg['downsample_voxel_size'], 
-                dbscan_remove_noise=cfg['dbscan_remove_noise'], 
-                dbscan_eps=cfg['dbscan_eps'], 
-                dbscan_min_points=cfg['dbscan_min_points'], 
-                spatial_sim_type=cfg['spatial_sim_type'], 
-                device=cfg['device']
+                downsample_voxel_size=self.configs.downsample_voxel_size, 
+                dbscan_remove_noise=self.configs.dbscan_remove_noise, 
+                dbscan_eps=self.configs.dbscan_eps, 
+                dbscan_min_points=self.configs.dbscan_min_points, 
+                spatial_sim_type=self.configs.spatial_sim_type, 
+                device=self.configs.device
                 # Note: Removed 'match_method' and 'phys_bias' as they do not appear in the provided merge function
             )
             map_edges = process_edges(match_indices, grounded_obs, len(objects), objects, map_edges)
 
-            is_final_frame = frame_idx == len(dataset) - 1
+            is_final_frame = frame_idx == len(self.dataset) - 1
             if is_final_frame:
                 print("Final frame detected. Performing final post-processing...")
 
             ### Perform post-processing periodically if told so
 
             # Denoising
-            if processing_needed(
-                cfg["denoise_interval"],
-                cfg["run_denoise_final_frame"],
-                frame_idx,
-                is_final_frame,
-            ):
-                objects = measure_time(denoise_objects)(
-                    downsample_voxel_size=cfg['downsample_voxel_size'], 
-                    dbscan_remove_noise=cfg['dbscan_remove_noise'], 
-                    dbscan_eps=cfg['dbscan_eps'], 
-                    dbscan_min_points=cfg['dbscan_min_points'], 
-                    spatial_sim_type=cfg['spatial_sim_type'], 
-                    device=cfg['device'], 
-                    objects=objects
-                )
+            if processing_needed(self.configs.denoise_interval,
+                                 self.configs.run_denoise_final_frame,
+                                 frame_idx,  
+                                 is_final_frame):
+                
+                objects = denoise_objects(downsample_voxel_size=self.configs.downsample_voxel_size, 
+                                          dbscan_remove_noise=self.configs.dbscan_remove_noise, 
+                                          dbscan_eps=self.configs.dbscan_eps, 
+                                          dbscan_min_points=self.configs.dbscan_min_points, 
+                                          spatial_sim_type=self.configs.spatial_sim_type, 
+                                          device=self.configs.device, 
+                                          objects=objects)
 
             # Filtering
-            if processing_needed(
-                cfg["filter_interval"],
-                cfg["run_filter_final_frame"],
-                frame_idx,
-                is_final_frame,
-            ):
-                objects = filter_objects(
-                    obj_min_points=cfg['obj_min_points'], 
-                    obj_min_detections=cfg['obj_min_detections'], 
-                    objects=objects,
-                    map_edges=map_edges
-                )
+            if processing_needed(self.configs.filter_interval,
+                                 self.configs.run_filter_final_frame,
+                                 frame_idx,
+                                 is_final_frame):
+                
+                objects = filter_objects(obj_min_points=self.configs.obj_min_points, 
+                                         obj_min_detections=self.configs.obj_min_detections, 
+                                         objects=objects,
+                                         map_edges=map_edges)
 
             # Merging
             if processing_needed(
@@ -480,13 +515,13 @@ class SceneGraph:
                     dbscan_remove_noise=cfg["dbscan_remove_noise"],
                     dbscan_eps=cfg["dbscan_eps"],
                     dbscan_min_points=cfg["dbscan_min_points"],
-                    spatial_sim_type=cfg["spatial_sim_type"],
+                    spatial_sim_type=self.configs.spatial_sim_type,
                     device=cfg["device"],
                     do_edges=cfg["make_edges"],
                     map_edges=map_edges
                 )
-            orr_log_objs_pcd_and_bbox(objects, obj_classes)
-            orr_log_edges(objects, map_edges, obj_classes)
+            orr_log_objs_pcd_and_bbox(objects, self.obj_classes)
+            orr_log_edges(objects, map_edges, self.obj_classes)
 
             if cfg.save_objects_all_frames:
                 save_objects_for_frame(
@@ -505,7 +540,7 @@ class SceneGraph:
                     exp_out_path=exp_out_path,
                     cfg=cfg,
                     objects=objects,
-                    obj_classes=obj_classes,
+                    obj_classes=self.obj_classes,
                     latest_pcd_filepath=cfg.latest_pcd_filepath,
                     create_symlink=True
                 )
